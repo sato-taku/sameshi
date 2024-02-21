9b7af52b4be2ba1ab6a106148183e61e170d4f4e
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-02-22 00:18:54 +0900

    トップページ作成

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,20 +7,6 @@ module.exports = {
   ],
   plugins: [require('daisyui')],
   daisyui: {
-    themes: [
-      {
-        mytheme: {
-          "primary": "#F5F5DC",        
-          "secondary": "#F5F5DC",
-          "accent": "#FFD700",
-          "neutral": "#FFD700",
-          "base-100": "#ffffff",
-          "info": "#C3B091",
-          "success": "#FFD700",
-          "warning": "#FFD700",
-          "error": "#FF6347",
-        },
-      },
-    ],
+    themes: ["bumblebee"],
   },
 }
