d11930ef3a7395ad77481e907e48d48b15802826
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-02-23 02:54:06 +0900

    フラッシュメッセージの追加

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -5,14 +5,15 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_back_or_to root_path
+      redirect_back_or_to root_path, success: t('user_sessions.create.success')
     else
-      render :new
+      flash[:danger] = t('user_sessions.create.failure')
+      render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
     logout
-    redirect_to root_path, status: :see_other
+    redirect_to root_path, status: :see_other, danger: t('user_sessions.destroy.success')
   end
 end
