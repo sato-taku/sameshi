3730aa7e3b25d9a9e8c916a8494dc738c89e9270
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-02-23 02:54:06 +0900

    フラッシュメッセージの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path
+      redirect_to root_path, success: t('users.create.success')
     else
-      render :new
+      flash[:danger] = t('users.create.failure')
+      render :new, status: :unprocessable_entity
     end
   end
 
