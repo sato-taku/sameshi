40533cd652e2df20bb82f2aeb78be38f0cf475f2
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-02-24 11:46:01 +0900

    Google Maps APIの導入

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_22_102141) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_22_183047) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "posts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "sauna_id", null: false
+    t.bigint "prefecture_id", null: false
+    t.string "meal_genre", null: false
+    t.text "content", null: false
+    t.string "post_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["prefecture_id"], name: "index_posts_on_prefecture_id"
+    t.index ["sauna_id"], name: "index_posts_on_sauna_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "prefectures", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "saunas", force: :cascade do |t|
+    t.string "name", null: false
+    t.decimal "latitude", precision: 10, scale: 7, null: false
+    t.decimal "longitude", precision: 10, scale: 7, null: false
+    t.string "address"
+    t.string "place_id", null: false
+    t.string "photo_reference"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -35,5 +60,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_22_102141) do
     t.index ["prefecture_id"], name: "index_users_on_prefecture_id"
   end
 
+  add_foreign_key "posts", "prefectures"
+  add_foreign_key "posts", "saunas"
+  add_foreign_key "posts", "users"
   add_foreign_key "users", "prefectures"
 end
