ab6165519b0bfa919d258dfc6b6f25bd83154908
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-03-07 21:02:40 +0900

    投稿一覧機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,6 @@ class PostsController < ApplicationController
   skip_before_action :require_login, only: %i[index]
   
   def index
-    @posts = Post.all
+    @posts = Post.all.includes(:user).order(created_at: :desc)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :sauna
   belongs_to :prefecture
+
+  validates :meal_genre, presence: true
+  validates :content, presence: true, length: { maximum: 1000 }
 end
