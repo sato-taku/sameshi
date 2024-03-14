234631cb4507286417bd854472af47cea7b5b2e9
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-03-15 03:08:43 +0900

    コメント作成・削除のAjax化

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   skip_before_action :require_login, only: %i[index]
+  before_action :set_post, only: %i[edit update destroy]
   
   def index
     @posts = Post.all.includes(:user).order(created_at: :desc)
@@ -14,7 +15,7 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to posts_path, success: t('defaults.flash_message.created', item: Post.model_name.human)
     else
-      flash['danger'] = t('defaults.flash_message.not_created', item: Post.model_name.human)
+      flash.now[:danger] = t('defaults.flash_message.not_created', item: Post.model_name.human)
       render :new, status: :unprocessable_entity
     end
   end
@@ -25,7 +26,27 @@ class PostsController < ApplicationController
     @comments = @post.comments.includes(:user).order(created_at: :desc)
   end
 
+  def edit; end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, success: t('defaults.flash_message.updated', item: Post.model_name.human)
+    else
+      flash.now[:danger] = t('defaults.flash_message.not_updated', item: Post.model_name.human)
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post.destroy!
+    redirect_to posts_path, status: :see_other, success: t('defaults.flash_message.deleted', item: Post.model_name.human)
+  end
+
   private
+
+  def set_post
+    @post = current_user.posts.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:sauna_id, :prefecture_id, :meal_genre, :content, :post_image, :post_image_cache)
