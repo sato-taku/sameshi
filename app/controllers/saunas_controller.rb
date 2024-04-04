13b0f844f3179c427e99592c54c737d20bb5d091
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-04-05 02:41:39 +0900

    update: autocompleteロジックの修正

--- app/controllers/saunas_controller.rb
+++ app/controllers/saunas_controller.rb
@@ -9,14 +9,7 @@ class SaunasController < ApplicationController
   end
 
   def autocomplete
-    @saunas = Sauna.where('name LIKE ?', "%#{params[:term]}%").limit(10)
+    @saunas = Sauna.where('name LIKE ?', "%#{params[:q]}%").limit(10)
     render partial: 'saunas/sauna', locals: { saunas: @saunas }
-  end
-
-  def search
-    @saunas = Sauna.search_by_name(params[:query]) if params[:query].present?
-    respond_to do |format|
-      format.json { render json: @saunas }
-    end
   end
 end
