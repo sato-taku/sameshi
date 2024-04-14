a6e89ce398555ea687c1fbcdfab077eed5890a05
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-04-14 15:46:08 +0900

    update: サウナ施設の範囲拡大

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
-  belongs_to :user
-  belongs_to :post
+  belongs_to :user, dependent: :destroy
+  belongs_to :post, dependent: :destroy
 
   validates :user_id, uniqueness: { scope: :post_id }
 end
