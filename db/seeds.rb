1b4f39035c9bc076cd855e2c2b16a9b003f09c2b
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-04-16 07:12:46 +0900

    fix: 本番環境のDB修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,34 +19,3 @@ prefectures = [
   '佐賀県', '長崎県', '熊本県', '大分県', '宮崎県', 
   '鹿児島県', '沖縄県'
 ]
-
-prefectures.each do |name|
-  Prefecture.find_or_create_by!(name: name)
-end
-
-niigata_prefecture = Prefecture.find_by(name: '新潟県')
-
-10.times do
-  User.create!(
-    nickname: Faker::Name.name,
-    email: Faker::Internet.email,
-    prefecture_id: niigata_prefecture.id,
-    age_group: User.age_groups.sample,
-    password: "password",
-    password_confirmation: "password"
-  )
-end
-
-10.times do
-  sauna = Sauna.all.sample
-  user = User.all.sample
-
-  Post.create!(
-    user_id: user.id,
-    sauna_id: sauna.id,
-    prefecture_id: niigata_prefecture.id,
-    meal_genre: Post.meal_genres.sample,
-    content: Faker::Lorem.sentence,
-    post_image: nil
-  )
-end
