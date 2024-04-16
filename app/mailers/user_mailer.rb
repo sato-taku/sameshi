deb6428f7f457e5efe9590d74ab2b5df007f2fc1
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-04-16 19:45:46 +0900

    fix: 本番環境でのパスワードリセット

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ApplicationMailer
-  default from: "from@example.com"
+  default from: "137meshi@gmail.com"
   
   def reset_password_email(user)
     @user = User.find(user.id)
-    @url  = edit_password_reset_url(@user.reset_password_token)
+    @url  = edit_password_reset_url(@user.reset_password_token, host: ActionMailer::Base.default_url_options[:host])
     mail(to: user.email,
          subject: t('defaults.password_reset'))
   end
