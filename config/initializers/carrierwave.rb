ddc29ff1f12718f1c0d65857147fd97fee78886a
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-04-17 21:08:03 +0900

    fix: 本番環境での画像表示

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['AWS_KEY'],
-    aws_secret_access_key: ENV['AWS_SEC_KEY'],
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
     region: 'ap-northeast-1'
   }
   config.fog_directory  = 'sameshi-image'
