9700576f52c7d4e25d3d79420ed1300a1b6a6dfb
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-04-17 21:46:29 +0900

    fix: 本番環境でのパスワードリセット

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'gmail.com',
+    domain:               'sameshi.fly.dev',
     user_name:            ENV['GMAIL_USERNAME'],
     password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
