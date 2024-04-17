7b6d91df57e1ac6db47d71bd7b3ad0ab726bc68a
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-04-17 15:51:46 +0900

    fix: パスワードリセット機能

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,12 +79,13 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'sameshi.fly.dev',
-    user_name:            ENV["GMAIL_USERNAME"],
-    password:             ENV["GMAIL_PASSWORD"],
+    domain:               'gmail.com',
+    user_name:            ENV['GMAIL_USERNAME'],
+    password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
-    enable_starttls_auto: true 
+    enable_starttls_auto: true
   }
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
