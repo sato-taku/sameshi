5aafd781c51812318d4097fb6bf81ad6cba93410
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-04-18 15:45:18 +0900

    update: UIの微調整

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,6 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
-
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
