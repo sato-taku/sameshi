60a12e61d0cf8b43dfa24b1e3ecd68ed4662e9ff
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-05-10 02:43:07 +0900

    add: ページネーションデザイン

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,11 +1,11 @@
 class ProfilesController < ApplicationController
   before_action :set_user, only: %i[show edit update]
+  before_action :set_recommend_posts, only: %i[show]
   def show
     @total_my_posts = @user.posts.count
     @total_my_likes = @user.like_posts.count
     @my_posts = @user.posts.includes(:user).order(id: :desc).page(params[:posts_page])
     @my_likes = @user.like_posts.includes(:user).order(id: :desc).page(params[:likes_page])
-    @recommend_posts = @user.recommend_posts
   end
   
   def edit; end
@@ -27,5 +27,9 @@ class ProfilesController < ApplicationController
 
   def user_params
     params.require(:user).permit(:nickname, :prefecture_id, :age_group, :email, :avatar, :avatar_cache)
+  end
+
+  def set_recommend_posts
+    @recommend_posts = current_user.recommend_posts
   end
 end
