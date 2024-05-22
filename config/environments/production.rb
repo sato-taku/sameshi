319aa92bbc48ce9ed83ffda06d698b0b1fedb020
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-05-22 13:08:22 +0900

    update: 独自ドメインの設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'sameshi-time.com',
+    domain:               'sameshi.fly.dev',
     user_name:            ENV['GMAIL_USERNAME'],
     password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
