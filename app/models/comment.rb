89c2d6c1ef68c7f52e6c8a174fce2be20a79e163
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-07-30 18:05:22 +0900

    add: 通知送信用コールバックメソッドを作成

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,19 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   validates :body, presence: true, length: { maximum: 150 }
+
+  after_create_commit :create_comment_notification
+
+  private
+
+  def create_comment_notification
+    return if self.user_id == self.commentable.user_id
+    if self.commentable
+      Notification.create(
+        sender_id: self.user_id,
+        recipient_id: self.commentable.user_id,
+        notifiable: self
+      )
+    end
+  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -3,4 +3,19 @@ class Like < ApplicationRecord
   belongs_to :post
 
   validates :user_id, uniqueness: { scope: :post_id }
+
+  after_create_commit :create_like_notification
+
+  private
+
+  def create_like_notification
+    return if self.user_id == self.likeable.user_id
+    if self.likeable
+      Notification.create(
+        sender_id: self.user_id,
+        recipient_id: self.likeable.user_id,
+        notifiable: self
+      )
+    end
+  end
 end
