037b1674e91c0a1700f0c0192ed1cff4b20aac9b
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-08-20 23:58:48 +0900

    update: 動画投稿機能(表示)

--- app/uploaders/post_image_uploader.rb
+++ app/uploaders/post_image_uploader.rb
@@ -20,24 +20,24 @@ class PostImageUploader < CarrierWave::Uploader::Base
   end
 
   # 画像を.webpに変換
-  process :convert_to_webp, if: :image?
+  # process :convert_to_webp, if: :image?
 
-  def convert_to_webp
-    manipulate! do |image|
-      image.format 'webp'
-      image
-    end
-  end
+  # def convert_to_webp
+  #   manipulate! do |image|
+  #     image.format 'webp'
+  #     image
+  #   end
+  # end
 
-  def image?
-    file && %w[jpg jpeg png gif heic].include?(file.extension.downcase)
-  end
+  # def image?
+  #   file && %w[jpg jpeg png gif heic].include?(file.extension.downcase)
+  # end
 
-  def filename
-    if original_filename.present? && image?
-      super.chomp(File.extname(super)) + '.webp'
-    else
-      super
-    end
-  end
+  # def filename
+  #   if original_filename.present? && image?
+  #     super.chomp(File.extname(super)) + '.webp'
+  #   else
+  #     super
+  #   end
+  # end
 end
