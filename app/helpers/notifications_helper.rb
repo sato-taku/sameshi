1cd0542b8241a54276298637723abfad015ffedd
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-09-06 05:09:06 +0900

    update: 通知機能

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -4,15 +4,15 @@ module NotificationsHelper
 
     case notification.notifiable_type
     when 'Comment'
-      "#{notification.sender.nickname} が #{generate_post_link(notification.notifiable.post)} にコメントしました".html_safe
+      "#{notification.sender.nickname} が 『 #{generate_post_link(notification.notifiable.post)} 』 にコメントしました".html_safe
 		when 'Like'
-      "#{notification.sender.nickname} が #{generate_post_link(notification.notifiable.post)} にいいね！しました".html_safe
+      "#{notification.sender.nickname} が 『 #{generate_post_link(notification.notifiable.post)} 』 にいいね！しました".html_safe
     else
       '新着通知がありました'
     end
   end
 
   def generate_post_link(post)
-    link_to post.sauna.name, post_path(post), class: "link link-primary", data: { turbo: false }
+    link_to post.sauna.name, post_path(post), class: "link hover:link-info link-bold", data: { turbo: false }
   end
 end
