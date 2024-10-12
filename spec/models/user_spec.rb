430e346d7a0f92dc59a6da5dd2bb750f3a1a3fae
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-10-12 21:52:48 +0900

    add: factory/prefecture

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
+  it 'ニックネーム、メール、年代、都道府県があり、パスワードは4文字以上であれば有効であること' do
+    user = build(:user)
+    expect(user).to be_valid
+  end
 end
