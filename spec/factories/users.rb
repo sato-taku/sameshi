08146bff0a082f5682b1a0ebc5ae8d3a332a005b
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-10-13 15:36:21 +0900

    add: ユーザー作成テスト追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     password { '12345678' }
     password_confirmation { '12345678' }
     age_group { User.age_groups.sample }
+    agreement { true }
     association :prefecture
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it 'ニックネーム、メール、年代、都道府県があり、パスワードは4文字以上であれば有効であること' do
-    user = build(:user)
+    user = create(:user)
     expect(user).to be_valid
   end
 end
