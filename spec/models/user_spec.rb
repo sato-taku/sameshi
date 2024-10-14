f2a61c071f47da41f3cbf05563db90a1fe2db126
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-10-15 00:16:58 +0900

    add: 必須項目のバリデーションテスト追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,4 +13,21 @@ RSpec.describe User, type: :model do
     user2.valid?
     expect(user2.errors[:email]).to include('はすでに存在します')
   end
+
+  it '必須項目を入力する必要があること' do
+    user = build(:user)
+    user.nickname = nil
+    user.prefecture_id = nil
+    user.age_group = nil
+    user.email = nil
+    user.password = nil
+    user.password_confirmation = nil
+    user.valid?
+    expect(user.errors[:nickname]).to include('を入力してください')
+    expect(user.errors[:prefecture_id]).to include('を入力してください')
+    expect(user.errors[:age_group]).to include('は一覧にありません')
+    expect(user.errors[:email]).to include('を入力してください')
+    expect(user.errors[:password]).to include('は4文字以上で入力してください')
+    expect(user.errors[:password_confirmation]).to include('を入力してください')
+  end
 end
