a0348202cf90f72d36258d0029fae6af16a46741
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-10-14 23:50:05 +0900

    add: メールアドレスのバリデーションテスト追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,12 @@ RSpec.describe User, type: :model do
     user = create(:user)
     expect(user).to be_valid
   end
+
+  it 'メールはユニークであること' do
+    user1 = create(:user)
+    user2 = create(:user)
+    user2.email = user1.email
+    user2.valid?
+    expect(user2.errors[:email]).to include('はすでに存在します')
+  end
 end
