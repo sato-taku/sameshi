b351642b1d3ffdb120256bc915bfa6b1eafe6144
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-10-15 16:25:11 +0900

    add: サ飯ジャンルのバリデーションテスト追加

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,4 +7,12 @@ RSpec.describe Post, type: :model do
       expect(post).to be_valid
     end
   end
+
+  context 'サ飯ジャンルが存在しない場合' do
+    it '無効であること' do
+      post = build(:post, meal_genre: nil)
+      expect(post).to be_invalid
+      expect(post.errors[:meal_genre]).to include('を入力してください')
+    end
+  end
 end
