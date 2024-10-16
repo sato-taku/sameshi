91b52af3a4888a32f0d2217f79381cb86b9c68ee
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-10-16 19:00:35 +0900

    add: ユニークバリデーションのテスト追加

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -7,4 +7,13 @@ RSpec.describe Like, type: :model do
       expect(like).to be_valid
     end
   end
+
+  context 'ユーザーと投稿の組み合わせがユニークでない場合' do
+    it '無効であること' do
+      like = create(:like)
+      new_like = build(:like, user: like.user, post: like.post)
+      new_like.valid?
+      expect(new_like.errors[:user_id]).to include('はすでに存在します'), 'postとuserのユニークバリデーションが設定されていません'
+    end
+  end
 end
