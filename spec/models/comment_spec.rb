3ce2ecb5ca7b4b4f346bd1048f30ad87fa0a8174
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-10-19 23:32:23 +0900

    add: 文字数バリデーション１のテスト追加

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -15,4 +15,11 @@ RSpec.describe Comment, type: :model do
       expect(comment.errors[:body]).to include('を入力してください')
     end
   end
+
+  context '本文が150文字以内の場合' do
+    it '有効であること' do
+      comment = build(:comment, body: 'n' * 150)
+      expect(comment).to be_valid
+    end
+  end
 end
