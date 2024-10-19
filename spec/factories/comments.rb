c223f8b70262fc62d55fd540c142cb81f0ece9a4
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-10-19 23:28:55 +0900

    add: bodyバリデーションのテスト追加

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :comment do
-    sequence(:body) { |n| "本文#{n} }
+    sequence(:body) { |n| "本文#{n}" }
     association :user
     association :post
   end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context '全てのフィールドが有効な場合' do
+    it '有効であること' do
+      comment = build(:comment)
+      expect(comment).to be_valid
+    end
+  end
+
+  context '本文が存在しない場合' do
+    it '無効であること' do
+      comment = build(:comment, body: nil)
+      expect(comment).to be_invalid
+      expect(comment.errors[:body]).to include('を入力してください')
+    end
+  end
 end
