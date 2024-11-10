0cd5f0e7994e59c56a4c782dae5e7c3bde0f0db3
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-11-10 20:24:56 +0900

    add: ログインテスト２追加

--- spec/system/user_sessions_spec.rb
+++ spec/system/user_sessions_spec.rb
@@ -22,6 +22,18 @@ RSpec.describe 'ログイン・ログアウト', type: :system do
           expect(page).to have_content('ログインしました'), 'フラッシュメッセージ「ログインしました」が表示されていません'
         end
       end
+
+      context 'PWに誤りがある場合' do
+        it 'ログインできないこと' do
+          visit '/login'
+          fill_in 'メールアドレス', with: general_user.email
+          fill_in 'パスワード', with: '1234'
+          click_button 'ログイン'
+          Capybara.assert_current_path("/login", ignore_query: true)
+          expect(current_path).to eq('/login'), 'ログイン失敗時にログイン画面に戻ってきていません'
+          expect(page).to have_content('ログインに失敗しました'), 'フラッシュメッセージ「ログインに失敗しました」が表示されていません'
+        end
+      end
     end
   end
 end
