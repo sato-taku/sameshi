6eaa96a53002376c070b7a9da3d7580340ebd9fb
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-11-18 20:05:55 +0900

    add: マイページ遷移テスト追加

--- spec/system/profiles_spec.rb
+++ spec/system/profiles_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "Profiles", type: :system do
+RSpec.describe "プロフィール", type: :system do
   let(:user) { create(:user) }
+  include LoginMacros
   before do
     login(user)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  it 'マイページ詳細が見られること' do
+    visit '/profile'
+    Capybara.assert_current_path("/profile", ignore_query: true)
+    expect(current_path).to eq('/profile'), 'マイページに遷移していません'
+    expect(page).to have_content(user.nickname), 'ニックネームが表示されていません'
+  end
 end
