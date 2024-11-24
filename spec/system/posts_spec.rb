cc41382be16d86ceb3fb197917df89d85ba99c93
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-11-24 15:47:46 +0900

    add: 投稿一覧テスト１追加

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -1,9 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
+RSpec.describe '投稿', type: :system do
+  let(:user) { create(:user) }
+  let(:another_user) { create(:user) }
+  let(:post) { create(:post) }
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe '投稿のCRUD' do
+    describe '投稿の一覧' do
+      context '投稿が一件もない場合' do
+        it '何もない旨のメッセージが表示されること' do
+          visit '/posts'
+          expect(page).to have_content('投稿がありません'), '投稿が一件もない場合「投稿がありません」というメッセージが表示されていません'
+        end
+      end
+    end
+  end
 end
