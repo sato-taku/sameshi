1968e97e342022d1f93996d93408b7c2e77548b6
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-11-25 19:55:28 +0900

    add: 投稿一覧テスト２追加

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -13,6 +13,16 @@ RSpec.describe '投稿', type: :system do
           expect(page).to have_content('投稿がありません'), '投稿が一件もない場合「投稿がありません」というメッセージが表示されていません'
         end
       end
+
+      context '投稿がある場合' do
+        it '投稿の一覧が表示されること' do
+          post
+          visit '/posts'
+          expect(page).to have_content(post.user.nickname), '投稿一覧画面に投稿者のニックネームが表示されていません'
+          # expect(page).to have_content(post.post_image), '投稿一覧画面に投稿画像が表示されていません'
+          # expect(page).to have_content(post.created_at), '投稿一覧画面に投稿時間が表示されていません'
+        end
+      end
     end
   end
 end
