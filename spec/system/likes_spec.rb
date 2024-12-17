00d0a3010932c3aca04ffd65e0f4eb594f15359f
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-12-17 11:19:51 +0900

    add: いいねテスト１追加

--- spec/system/likes_spec.rb
+++ spec/system/likes_spec.rb
@@ -2,8 +2,18 @@ require 'rails_helper'
 
 RSpec.describe "いいね", type: :system do
   let(:user) { create(:user) }
+  let!(:sauna) { create(:sauna) }
   let!(:post) { create(:post) }
   let!(:like) { create(:like, user: user) }
+  include LoginMacros
 
+  it 'いいねができること' do
+    login(user)
+    visit '/posts'
+    find("a[href='#{post_path(post)}']").click
+    find("#like-button-for-post-#{post.id}").click
+    expect(current_path).to eq("/posts/#{post.id}")
+    expect(page).to have_selector("#unlike-button-for-post-#{post.id}")
+  end
 
 end
