687f6395dfb536999418b92d1bca0cf489c53c31
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-12-20 11:25:46 +0900

    add: コメント一覧テスト１追加

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -3,7 +3,23 @@ require 'rails_helper'
 RSpec.describe "Comments", type: :system do
   let(:me) { create(:user) }
   let(:post) { create(:post) }
+  let(:sauna) { create(:sauna) }
   let!(:comment_by_me) { create(:comment, user: me, post: post) }
   let!(:comment_by_others) { create(:comment, post: post) }
   include LoginMacros
+
+  describe 'コメントのCRUD' do
+    before do
+      login(me)
+      visit '/posts'
+      find("a[href='#{post_path(post)}']").click
+    end
+
+    describe 'コメントの一覧' do
+      it 'コメントの一覧が表示されること' do
+        expect(page).to have_content(comment_by_me.body)
+        expect(page).to have_content(comment_by_me.user.nickname)
+      end
+    end
+  end
 end
