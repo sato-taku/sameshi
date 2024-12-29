12ea404aeddcc0eb10979b93fc56887aebdf418d
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-12-29 22:08:22 +0900

    add: 投稿検索テスト４追加

--- spec/system/searches_spec.rb
+++ spec/system/searches_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe "Searches", type: :system do
   let(:user) { create(:user) }
   let(:sauna) { create(:sauna) }
-  let(:post1) { create(:post, content: 'ポスト１') }
-  let(:post2) { create(:post, content: 'ポスト２') }
-  let(:post3) { create(:post, content: 'ポスト３') }
-  let(:post4) { create(:post, content: 'ポスト４') }
+  let!(:post1) { create(:post, content: 'ポスト１') }
+  let!(:post2) { create(:post, content: 'ポスト２') }
+  let!(:post3) { create(:post, content: 'ポスト３', meal_genre: 'ラーメン') }
+  let!(:post4) { create(:post, content: 'ポスト４') }
   include LoginMacros
 
   describe '投稿一覧画面での検索' do
@@ -37,6 +37,16 @@ RSpec.describe "Searches", type: :system do
           expect(page).to have_content(post2.user.nickname)
           # expect(page).to have_content(post3.user.nickname)
           # expect(page).to have_content(post4.user.nickname)
+        end
+      end
+
+      describe 'ジャンルでの検索機能の検証' do
+        it '該当する投稿のみ表示されること' do
+          select 'ラーメン', from: 'post_meal_genre_eq'
+          find("button[type='submit']").click
+          Capybara.assert_current_path("/posts", ignore_query: true)
+          expect(current_path).to eq('/posts')
+          expect(page).to have_content(post3.user.nickname)
         end
       end
     end
