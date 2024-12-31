e0fd302c1c354890dcdb3232ef0d24dfb35af63f
sato-taku <142493716+sato-taku@users.noreply.github.com>
2024-12-31 21:18:02 +0900

    add: 共通系テストヘッダー１追加

--- spec/system/commons_spec.rb
+++ spec/system/commons_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe "共通系", type: :system do
     before do
       visit root_path
     end
+    describe 'ヘッダー' do
+      it '通知が表示されていないこと' do
+        expect(page).not_to have_selector('.dropdown.fixed.top-4.left-2.md\:left-4.z-50')
+      end
+    end
+  end
 end
