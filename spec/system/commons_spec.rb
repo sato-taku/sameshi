ec6ce9c70478c21a038db416f5f756181cbcc6d3
sato-taku <142493716+sato-taku@users.noreply.github.com>
2025-01-06 18:48:13 +0900

    add: タイトルテスト１追加

--- spec/system/commons_spec.rb
+++ spec/system/commons_spec.rb
@@ -12,17 +12,23 @@ RSpec.describe "共通系", type: :system do
       it '通知が表示されていないこと' do
         expect(page).not_to have_selector('.dropdown.fixed.top-4.left-2.md\:left-4.z-50')
       end
-      
-      describe 'ハンバーガーメニュー' do
-        it 'ログインボタンが表示されていること' do
-          find('.drawer-button.btn.btn-ghost').click
-          expect(page).to have_content('ログイン')
-        end
+    end
+    
+    describe 'ハンバーガーメニュー' do
+      it 'ログインボタンが表示されていること' do
+        find('.drawer-button.btn.btn-ghost').click
+        expect(page).to have_content('ログイン')
+      end
 
-        it '新規登録ボタンが表示されていること' do
-          find('.drawer-button.btn.btn-ghost').click
-          expect(page).to have_content('新規登録')
-        end
+      it '新規登録ボタンが表示されていること' do
+        find('.drawer-button.btn.btn-ghost').click
+        expect(page).to have_content('新規登録')
+      end
+    end
+
+    describe 'タイトル' do
+      it 'タイトルが正しく表示されていること' do
+        expect(page).to have_title('サ飯の時間')
       end
     end
   end
