6e9f237003eac4c6d96c1384a5376867efad0302
sato-taku <142493716+sato-taku@users.noreply.github.com>
2025-01-12 10:57:12 +0900

    add: ヘッダーテスト３追加

--- spec/system/commons_spec.rb
+++ spec/system/commons_spec.rb
@@ -55,6 +55,13 @@ RSpec.describe "共通系", type: :system do
       it '通知が表示されていること' do
         expect(page).to have_selector('.dropdown.fixed.top-4.left-2.md\:left-4.z-50')
       end
+
+      it 'ロゴを押下するとトップページに遷移すること' do
+        visit 'posts_path'
+        find('#header-logo').click
+        Capybara.assert_current_path("/", ignore_query: true)
+        expect(current_path).to eq('/')
+      end
     end
 
     describe 'ハンバーガーメニュー' do
