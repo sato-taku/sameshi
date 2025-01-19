eef60838934e84b9d26308feb05c8a3e320c9368
sato-taku <142493716+sato-taku@users.noreply.github.com>
2025-01-19 22:48:18 +0900

    add: タイトルテスト追加

--- spec/system/password_resets_spec.rb
+++ spec/system/password_resets_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe "PasswordResets", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
+RSpec.describe "パスワードリセット", type: :system do
+  let(:user) { create(:user) }
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe 'タイトル' do
+    describe 'パスワードリセット申請ページ' do
+      it '正しいタイトルが表示されていること' do
+        visit new_password_reset_path
+        expect(page).to have_title('パスワードリセット申請')
+      end
+    end
+  end
 end
