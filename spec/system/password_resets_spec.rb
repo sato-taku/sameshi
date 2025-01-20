34168a1ddb21c35b19b94ea60dd3874b564300d6
sato-taku <142493716+sato-taku@users.noreply.github.com>
2025-01-20 22:10:37 +0900

    add: タイトルテスト２追加

--- spec/system/password_resets_spec.rb
+++ spec/system/password_resets_spec.rb
@@ -10,5 +10,13 @@ RSpec.describe "パスワードリセット", type: :system do
         expect(page).to have_title('パスワードリセット申請')
       end
     end
+
+    describe 'パスワードリセットページ' do
+      it '正しいタイトルが表示されていること' do
+        user.generate_reset_password_token!
+        visit edit_password_reset_path(user.reset_password_token)
+        expect(page).to have_title('パスワードリセット')
+      end
+    end
   end
 end
