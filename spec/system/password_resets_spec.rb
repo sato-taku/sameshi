1655f2958323306a27c9a4c9bbff50c920f1b7ba
sato-taku <142493716+sato-taku@users.noreply.github.com>
2025-01-21 22:15:37 +0900

    add: パスワードリセットテスト追加

--- spec/system/password_resets_spec.rb
+++ spec/system/password_resets_spec.rb
@@ -19,4 +19,18 @@ RSpec.describe "パスワードリセット", type: :system do
       end
     end
   end
+
+  it 'パスワード変更ができる' do
+    visit new_password_reset_path
+    fill_in 'メールアドレス', with: user.email
+    click_button '送信'
+    expect(page).to have_content('パスワードリセット手順を送信しました')
+    visit edit_password_reset_path(user.reload.reset_password_token)
+    fill_in 'パスワード', with: '12345678'
+    fill_in 'パスワード（確認用）', with: '12345678'
+    click_button '更新'
+    Capybara.assert_current_path("/login", ignore_query: true)
+    expect(current_path).to eq('/login')
+    expect(page).to have_content('パスワードを変更しました')
+  end
 end
